86209e426fd3317452873ce41d6e10a1ca6892c7
White Nguyen <whitenguyen0909@gmail.com>
2022-05-04 07:44:40 -0600

    added react-gh-pages to deploy

---
+++ precache-manifest.fc9250280f1373821020ecea822ada63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3be551020fd9034dec4fa0a426f06ccb",
+    "url": "/rendering-with-react/index.html"
+  },
+  {
+    "revision": "40a7b521fea358e620ac",
+    "url": "/rendering-with-react/static/css/main.7dffbf4d.chunk.css"
+  },
+  {
+    "revision": "27c75f56849dfb2ceb90",
+    "url": "/rendering-with-react/static/js/2.cdabfff5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/rendering-with-react/static/js/2.cdabfff5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40a7b521fea358e620ac",
+    "url": "/rendering-with-react/static/js/main.08952e5d.chunk.js"
+  },
+  {
+    "revision": "3a3933175bacd1c5b5e8",
+    "url": "/rendering-with-react/static/js/runtime-main.95c6181c.js"
+  }
+]);
