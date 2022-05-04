ac9ef8ed91f8a59f17eb8f81b5f8eb12a1a38d84
White Nguyen <whitenguyen0909@gmail.com>
2022-05-04 07:23:58 -0600

    moved index.js to front

--- src/Activity.js
+++ src/Activity.js
@@ -1,24 +1,24 @@
 import React from "react";
 import "./Activity.css";
 
-// function Activity(props) {
-//   return (
-//     <tr key={props.key}>
-//       <td className="time">{props.time}</td>
-//       <td className="description">{props.description}</td>
-//     </tr>
-//   );
-// }
-
-//or
-
-function Activity({ key, time, description }) {
+function Activity(props) {
   return (
-    <tr key={key}>
-      <td className="time">{time}</td>
-      <td className="description">{description}</td>
+    <tr key={props.index}>
+      <td className="time">{props.time}</td>
+      <td className="description">{props.description}</td>
     </tr>
   );
 }
+
+//or
+
+// function Activity({ index, time, description }) {
+//   return (
+//     <tr key={index}>
+//       <td className="time">{time}</td>
+//       <td className="description">{description}</td>
+//     </tr>
+//   );
+// }
 
 export default Activity;
