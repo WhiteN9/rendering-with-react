401f6eb6c73fd5fa8aef79bd2843a0aff153bcb6
White Nguyen <whitenguyen0909@gmail.com>
2022-05-03 21:10:20 -0600

    return a hobby list using inline embedded expression

--- src/HobbyList.js
+++ src/HobbyList.js
@@ -1,8 +1,21 @@
 import React from "react";
 import "./HobbyList.css";
 
-function HobbyList() {
-  return <h4>Hobbies</h4>; // You must use this heading for this component
+
+function HobbyList({hobbies}) {
+  if (hobbies.length ===0) return null;
+  return (
+    <div className="hobbies">
+      <h4>Hobbies</h4>
+      <ul>
+        {hobbies.map((hobby,index) => <li key={index}>{hobby}</li>
+        )}
+      </ul>
+    </div>
+
+  ) 
+  // You must use this heading for this component
 }
 
 export default HobbyList;
+
